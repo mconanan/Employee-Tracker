f843be005b304352951574ffe9ed458fe232bd3c
madeleine <mconanan@gmail.com>
2022-10-04 22:00:34 -0700

    code functional; need to update readme & film screencastify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
-const Employee = require("./employee");
-const Role = require("./role");
+
+
 
 // CREATE A CONNECTION TO THE DATABASE
 const db = mysql.createConnection({
@@ -35,34 +35,7 @@ const start = [
   },
 ];
 
-// CREATE AN EMPLOYEE QUESTIONS
-const employeeQuestions = [
-  {
-    type: "input",
-    message: "What is the employee's first name?",
-    name: "first_name",
-    default: "First Name",
-  },
-  {
-    type: "input",
-    message: "What is the employee's last name?",
-    name: "last_name",
-    default: "Last Name",
-  },
-  {
-    type: "list",
-    message: "What is the employee's role?",
-    name: "employee_role",
-    default: "Employee Role",
-  },
-  {
-    type: "input",
-    message: "Who is the employee's manager?",
-    name: "manager",
-    default: "Manager",
-  },
-];
-
+// VARIABLE FOR QUESTION TO ADD A DEPARTMENT
 const addDeptRow = {
   type: "input",
   message: "What is the name of the department you would like to add?",
@@ -70,17 +43,15 @@ const addDeptRow = {
   default: "New Department",
 };
 
-
 //FUNCTION TO INITIATE INQUIRER
 function init() {
   return inquirer.prompt(start).then((answer) => {
-    console.log("start action", answer);
-
+    // SWITCH CASE FOR ALL ANSWERS TO FIRST QUESTION
     switch (answer.action) {
       case "view all departments":
         db.query("SELECT * FROM all_departments", function (err, results) {
           console.table(results);
-          console.log(results);
+
           init();
         });
 
@@ -113,9 +84,6 @@ function init() {
         addEmployee();
         break;
 
-      case "update an employee role":
-        updateEmployeeRole();
-        break;
       default:
     }
   });
@@ -128,7 +96,7 @@ const addDepartment = () => {
       "INSERT INTO all_departments (department_name) VALUES('" +
       answer.new_department +
       "')";
-    console.log("dept", deptSql);
+    
     db.query(deptSql, function (err, results) {
       if (err) {
         console.log(err);
@@ -140,99 +108,196 @@ const addDepartment = () => {
   });
 };
 
-// CREATING ARRAY OF DEPARTMENTS FOR INQUIRER QUESTION
-var departmentChoices;
-db.query("SELECT * FROM all_departments", function (err, results) {
-  if (err) {
-    console.error(err);
-  }
-  console.table(results);
+// CREATING ARRAY OF DEPARTMENTS FOR ADD ROLE INQUIRER PROMPTS
+const getAllDepts = () => {
+  return new Promise((resolve, reject) => {
+    var departmentChoices;
+    db.query("SELECT * FROM all_departments", function (err, results) {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      console.table(results);
 
-  departmentChoices = results.map((department) => {
-    console.log('department', department);
-    return {name: department.department_name, value: department.id};
+      departmentChoices = results.map((department) => {
+        return { name: department.department_name, value: department.id };
+      });
+
+      resolve(departmentChoices);
+    });
   });
-
-  console.log(departmentChoices);
-});
+};
 
 // ADD ROLE FUNCTION
 const addRole = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        message: "What is the name of the role you would like to add?",
-        name: "new_role",
-        default: "New Role",
-      },
-      {
-        type: "list",
-        message: "Which department is the new role under?",
-        choices: departmentChoices,
-        name: "role_department",
-        default: "New Role Department",
-      },
-      {
-        type: "input",
-        message: "What is the salary of the role you would like to add?",
-        name: "role_salary",
-        default: "New Role Salary",
-      },
-    ])
-    .then((answers) => {
-      const role = [`'${answers.new_role}'`, answers.role_department, parseFloat(answers.role_salary),].join(",");
+  getAllDepts().then((departmentChoices) => {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the role you would like to add?",
+          name: "new_role",
+          default: "New Role",
+        },
+        {
+          type: "list",
+          message: "Which department is the new role under?",
+          choices: departmentChoices,
+          name: "role_department",
+          default: "New Role Department",
+        },
+        {
+          type: "input",
+          message: "What is the salary of the role you would like to add?",
+          name: "role_salary",
+          default: "New Role Salary",
+        },
+      ])
+      .then((answers) => {
+        const role = [
+          `'${answers.new_role}'`,
+          answers.role_department,
+          parseFloat(answers.role_salary),
+        ].join(",");
 
-      console.log("role", role);
 
-      const roleSql =
-        "INSERT INTO all_roles (job_title, department_id, salary) VALUES(" +
-        role +
-        ")";
+        const roleSql =
+          "INSERT INTO all_roles (job_title, department_id, salary) VALUES(" +
+          role +
+          ")";
 
-      db.query(roleSql, function (err, results) {
-        if (err) {
-          console.log(err);
-        }
+        db.query(roleSql, function (err, results) {
+          if (err) {
+            console.log(err);
+          }
 
-        console.table(results);
+          console.table(results);
 
-        init();
+          init();
+        });
       });
-    });
+  });
 };
 
-//ADD EMPLOYEE FUNCTION
-const addEmployee = () => {
-  return inquirer.prompt(employeeQuestions).then((answers) => {
-    const employee = new Employee(
-      answers.first_name,
-      answers.last_name,
-      answers.job_title,
-      answers.deparment,
-      answers.salary,
-      answers.manager_id
-    );
+// CREATING ARRAYS TO USE FOR ADD EMPLOYEES INQUIRER PROMPTS
+const getAllManagers = () => {
+  return new Promise((resolve, reject) => {
+    var managerChoices;
+    db.query(
+      "SELECT first_name, id FROM all_employees",
+      function (err, results) {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        }
+        console.table(results);
 
-    const employeeSql =
-      "INSERT INTO all_employees (first_name, last_name, job_title, deparment, salary, manager_id) VALUES('" +
-      employee.getInfo() +
-      "')";
+        managerChoices = results.map((manager) => {
+          console.log("manager ID", manager);
+          return { name: manager.first_name, value: manager.id };
+        });
 
-    db.query(employeeSql, function (err, results) {
-      if (err) {
-        console.log(err);
+        resolve(managerChoices);
       }
+    );
+  });
+};
 
+const getAllRoles = () => {
+  return new Promise((resolve, reject) => {
+    var roleChoices;
+    db.query("SELECT job_title, id FROM all_roles", function (err, results) {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
       console.table(results);
 
-      init();
+      roleChoices = results.map((role) => {
+        return { name: role.job_title, value: role.id };
+      });
+
+      resolve(roleChoices);
     });
+  });
+};
+
+//ADD EMPLOYEE FUNCTION USING THE GETALLROLES & GETALLMANAGERS FUNCTIONS TO POPULATE ARRAYS FOR QUESTION CHOICES
+const addEmployee = () => {
+  var addEmployeeAnswers = {};
+  getAllRoles().then((roleChoices) => {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the employee's first name?",
+          name: "first_name",
+          default: "First Name",
+        },
+        {
+          type: "input",
+          message: "What is the employee's last name?",
+          name: "last_name",
+          default: "Last Name",
+        },
+        {
+          type: "list",
+          message: "What is the employee's role?",
+          choices: roleChoices,
+          name: "employee_role",
+          default: "Employee Role",
+        },
+      ])
+      .then((answers) => {
+        
+
+        addEmployeeAnswers.first_name = answers.first_name;
+        addEmployeeAnswers.last_name = answers.last_name;
+        addEmployeeAnswers.employee_role = answers.employee_role;
+
+        getAllManagers()
+          .then((managerChoices) => {
+            return inquirer
+              .prompt([
+                {
+                  type: "list",
+                  message: "Who is the employee's manager?",
+                  choices: managerChoices,
+                  name: "manager",
+                  default: "Manager",
+                },
+              ])
+              .then((answers) => {
+                return answers;
+              });
+          })
+          .then((answers) => {
+            const employee = [
+              `'${addEmployeeAnswers.first_name}',
+            '${addEmployeeAnswers.last_name}',
+            '${addEmployeeAnswers.employee_role}',
+            '${answers.manager}',`,
+            ];
+
+            console.log('addemployeeanswers', addEmployeeAnswers)
+
+            const employeeSql = `INSERT INTO all_employees (first_name, last_name, role_id, manager_id) VALUES("${addEmployeeAnswers.first_name}","${addEmployeeAnswers.last_name}", "${addEmployeeAnswers.employee_role}", "${answers.manager}");`;
+
+            db.query(employeeSql, function (err, results) {
+              if (err) {
+                console.log(err);
+              }
+
+              console.table(results);
+
+              init();
+            });
+          });
+      });
   });
 };
 
 // CALLS INQUIRER START FUNCTION
 init();
 
-// TO DO:
-// - HOW TO ASK, "WHICH DEPARTMENT IS THE NEW ROLE UNDER?"
+
